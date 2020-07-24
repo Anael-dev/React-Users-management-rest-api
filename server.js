294f47d5b18e8bba4ec96208622b8b343a24d281
anael <anael.mashinsky@gmail.com>
2020-07-24 23:05:39 +0300

    server.js changes

--- server.js
+++ server.js
@@ -18,23 +18,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(favicon(__dirname + "client/build/favicon.ico"));
+  //   app.use(favicon(__dirname + "client/build/favicon.ico"));
   // the __dirname is the current directory from where the script is running
-  app.use(express.static(__dirname));
-  app.use(express.static(path.join(__dirname, "client/build")));
+  //   app.use(express.static(__dirname));
+  app.use(express.static("client/build"));
 
   app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-
-// Handle React routing, return all requests to React app
-//   app.get("*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
-//   });
-// }
 
 app.use("/api/users", membersRouter);
 app.use("/api/posts", postsRouter);
