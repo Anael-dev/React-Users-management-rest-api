a5360e46f7709475c840b5a186a456017792f427
anael <anael.mashinsky@gmail.com>
2020-07-25 12:08:14 +0300

    Version 1.0.0

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ let express = require("express");
 var cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
-const favicon = require("express-favicon");
-const port = process.env.PORT || 8000;
 
 var membersRouter = require("./routers/membersRoute");
 var postsRouter = require("./routers/postsRoute");
@@ -17,17 +15,20 @@ require("./configs/database");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use("/api/users", membersRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/todos", todosRouter);
+
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static("client/build"));
+
   // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
-app.use("/api/users", membersRouter);
-app.use("/api/posts", postsRouter);
-app.use("/api/todos", todosRouter);
+const port = process.env.PORT || 8000;
 
 app.listen(port, console.log(`Server is starting at ${port}`));
